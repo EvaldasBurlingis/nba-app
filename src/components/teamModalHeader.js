1fc78a49c4a17c4c193e08270787c1d28449869f
Evaldas <evabur@icloud.com>
2019-07-23 12:39:32 +0100

    tabs

--- src/components/teamModalHeader.js
+++ src/components/teamModalHeader.js
@@ -1,11 +1,92 @@
 import React from "react";
+import PropTypes from "prop-types";
+// UI IMPORTS
+import { makeStyles } from "@material-ui/core/styles";
+import ClearIcon from "@material-ui/icons/Clear";
 
-const TeamModalHeader = ({ handleClose }) => {
-    return (
-        <div>
-            <button onClick={handleClose}>X</button>
-        </div>
-    )
+// Tabs test
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import PhoneIcon from "@material-ui/icons/Phone";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-force-tabpanel-${index}`}
+      aria-labelledby={`scrollable-force-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
 }
 
-export default TeamModalHeader
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-force-tab-${index}`,
+    "aria-controls": `scrollable-force-tabpanel-${index}`
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    backgroundColor: theme.palette.background.paper
+  },
+  clearIcon: {
+    fontSize: "2rem"
+  }
+}));
+
+const TeamModalHeader = ({ handleClose }) => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+  return (
+    <div>
+      <ClearIcon classsName={classes.clearIcon} onClick={handleClose} />
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons="on"
+            indicatorColor="primary"
+            textColor="primary"
+            aria-label="Scrollable force tabs example"
+          >
+            <Tab label="Item One" icon={<PhoneIcon />} {...a11yProps(0)} />
+            <Tab label="Item two" icon={<PhoneIcon />} {...a11yProps(1)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item two
+        </TabPanel>
+      </div>
+    </div>
+  );
+};
+
+export default TeamModalHeader;
