5fab2cde7c78fefb82dfbaffd740e6be20a87ef7
Evaldas <evabur@icloud.com>
2019-07-24 00:03:33 +0100

    modal scroll fixed

--- src/components/teamModal.js
+++ src/components/teamModal.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { TeamModalHeader } from "./index";
+import { enableBodyScroll, disableBodyScroll } from "body-scroll-lock";
 // UI IMPORTS
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
@@ -22,13 +23,29 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const TeamModal = ({ isOpen, handleClose, team }) => {
+  const scrollRef = useRef();
   const classes = useStyles();
+
+  useEffect(() => {
+    if (isOpen) {
+      disableBodyScroll(scrollRef.current);
+    } else {
+      enableBodyScroll(scrollRef.current);
+    }
+    return () => enableBodyScroll(scrollRef.current);
+  }, [isOpen]);
   return (
     <Modal
+      ref={scrollRef}
       aria-labelledby="simple-modal-title"
       aria-describedby="simple-modal-description"
       open={isOpen}
       onClose={handleClose}
+      style={{
+        overflowY: "auto",
+        WebkitOverflowScrolling: "touch",
+        transition: "opacity 300ms"
+      }}
     >
       <div className={classes.paper}>
         <TeamModalHeader handleClose={handleClose} team={team} />
